e1ad7223613a025e1b8b1df3ee28357419999a42
jswan <jswannieman@gmail.com>
2023-08-28 10:42:03 +1200

    Update UpdateEffectComponent  Javascript Component

--- src/UpdateEffectComponent.js
+++ src/UpdateEffectComponent.js
@@ -1,20 +1,23 @@
 import { useState } from "react";
+import useUpdateEffect from "./useUpdateEffect";
 
 const UpdateEffectComponent = () => {
   const [counter1, setCounter1] = useState(25);
   const [counter2, setCounter2] = useState(0);
 
-  useUpdateEffect(() => setCounter2((prevCounter1) => prevCounter1 * 4));
+  useUpdateEffect(() => setCounter2(counter1 * 4), [counter1]);
 
   return (
     <>
-      <div>{counter1}</div>
-      <button
-        onClick={() => setCounter1((currentCounter1) => currentCounter1 + 1)}
-      >
-        Increment Counter1
-      </button>
-      <div>{counter2}</div>
+      <div className="container">
+        <div className="counter1">Counter1 : {counter1}</div>
+        <button
+          onClick={() => setCounter1((currentCounter1) => currentCounter1 + 1)}
+        >
+          Increment Counter1
+        </button>
+        <div className="counter2">Counter2 : {counter2}</div>
+      </div>
     </>
   );
 };
